3817497bd0f00fcfd01f9b6f5479430ff9bc520f
nero19960329 <wangzhao2013tsinghua@gmail.com>
2015-07-07 15:36:57 +0800

    version 1.2.2
    maybe final changes about rd

--- jq.js
+++ jq.js
@@ -1,30 +1,35 @@
 $(function () {
-	var faceHeight = $("header #face").innerHeight();
-	$("header #face").css('width', faceHeight);
-	$("header #htmltitle").css('left', faceHeight * 1.3);
-
 	var width = $(window).width();
 	var height = $(window).height();
+	var faceHeight = $("header #face").innerHeight();
+	$("header #face").css('width', faceHeight);
+	$("header #htmltitle").css('left', faceHeight * 1.4);
+	$("header #introduction").css('left', faceHeight * 1.4);
+	$("header #social").css('left', width - 300);
 
 	if (height < 500 && height >= 300) {
 		$("html").css('font-size', 12);
 	} else if (height < 300) {
 		$("html").css('font-size', 9);
+	} else if (height >= 700) {
+		$("html").css('font-size', 16);
 	}
-	alert(height);
 })
 
 $(window).resize(function() {
-	var faceHeight = $("header #face").innerHeight();
-	$("header #face").css('width', faceHeight);
-	$("header #htmltitle").css('left', faceHeight * 1.3);
-
 	var width = $(window).width();
 	var height = $(window).height();
+	var faceHeight = $("header #face").innerHeight();
+	$("header #face").css('width', faceHeight);
+	$("header #htmltitle").css('left', faceHeight * 1.4);
+	$("header #introduction").css('left', faceHeight * 1.4);
+	$("header #social").css('left', width - 300);
 
 	if (height < 500 && height >= 300) {
 		$("html").css('font-size', 12);
 	} else if (height < 300) {
 		$("html").css('font-size', 9);
+	} else if (height >= 700) {
+		$("html").css('font-size', 16);
 	}
 });
