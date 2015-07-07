ef5665d7485682bfc54348b90240597a248f8dcc
nero19960329 <wangzhao2013tsinghua@gmail.com>
2015-07-07 19:15:53 +0800

    version 1.2.4
    test

--- jq.js
+++ jq.js
@@ -6,6 +6,19 @@ $(function () {
 	$("header #htmltitle").css('left', faceHeight * 1.4);
 	$("header #introduction").css('left', faceHeight * 1.4);
 	$("header #social").css('left', width - 300);
+	var htmlHeight = $(document).height();
+	var bodyHeight = $("body").height();
+	//var DocumentHeight = $(document.body).outerHeight(true);
+	//$("footer").css('top', DocumentHeight * 0.95);
+	//alert($(document).outerHeight(true));
+	//alert($("article").height());
+	//alert(height + " " + htmlHeight + " " + bodyHeight);
+	
+	var contactme_margin = $("article #contact-me").width();
+	if (contactme_margin != undefined) {
+		$("article").css('margin-left', (width - 160) / 2);
+		$("article").css('margin-right', (width - 160) / 2);
+	}
 
 	if (height < 500 && height >= 300) {
 		$("html").css('font-size', 12);
@@ -24,6 +37,8 @@ $(window).resize(function() {
 	$("header #htmltitle").css('left', faceHeight * 1.4);
 	$("header #introduction").css('left', faceHeight * 1.4);
 	$("header #social").css('left', width - 300);
+	var htmlHeight = $(document).height();
+	$("footer").css('top', htmlHeight);
 
 	if (height < 500 && height >= 300) {
 		$("html").css('font-size', 12);
