405b3efedc1ef871fa29e1784eebe37a9d072103
nero19960329 <wangzhao2013tsinghua@gmail.com>
2015-07-10 16:04:02 +0800

    version 1.3.6
    arrange files

--- js/jq.js
+++ js/jq.js
@@ -20,14 +20,16 @@ function setType() {
 
 	var headerHeight = $("header").height();
 	var headerOffsettop = $("header").offset().top;
-
 	var width = $(window).width();
 	var height = $(window).height();
 	var htmlHeight = $(document).height();
 	var bodyHeight = $("body").height();
 	var bodyWidth = $("body").width();
+	console.log($("html").attr('class'));
 	if (width > 960) {
-		$('article').css('width', 0.8 * width - 110);
+		if ($("html").attr('class') != "blogs") {
+			$('article').css('width', 0.8 * width - 110);
+		}
 	} else {
 		var faceHeight = $("header #face").innerHeight();
 		$("header #face").css('width', faceHeight);
@@ -36,25 +38,12 @@ function setType() {
 		$("article").css('margin-top', headerHeight + headerOffsettop + height * 0.04);
 		$("header").css('width', bodyWidth);
 	}
-	//$("article").css('margin-top', headerHeight + headerOffsettop + height * 0.04);
-	//$("aside").css('margin-top', headerHeight + headerOffsettop + height * 0.04);
-	
-	
-	//$("header #social").css('left', width - 320);
 	
 	var contactme_margin = $("article #contact-me").width();
 	if (contactme_margin != undefined) {
 		$("article").css('margin-left', (width - 160) / 2);
 		$("article").css('margin-right', (width - 160) / 2);
 	}
-
-	/*if (height < 500 && height >= 300) {
-		$("html").css('font-size', 12);
-	} else if (height < 300) {
-		$("html").css('font-size', 9);
-	} else if (height >= 700) {
-		$("html").css('font-size', 16);
-	}*/
 }
 
 $(function () {
