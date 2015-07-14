9580a5fd99b50991d2628db51a2fd4e480ceeddf
nero19960329 <wangzhao2013tsinghua@gmail.com>
2015-07-14 16:37:11 +0800

    version 1.5.4
    test for waterfall

---
+++ js/displaywindow.js
@@ -0,0 +1,273 @@
+$(window).bind({
+	resize: function() {
+		$('#fog').css('width', $(window).width());
+		$('#fog').css('height', $(window).height());
+	}
+});
+
+function DisplayWindow(arg) {
+	var mWindowWidth = $(window).width() * 0.9;
+	var mWindowHeight;
+	var changedWidth = arg.width;
+	var changedHeight = arg.height;
+	if (arg.width > mWindowWidth * 0.75) {
+		changedWidth = mWindowWidth * 0.75 - 20;
+		changedHeight = arg.height * (mWindowWidth * 0.75 - 20) / arg.width;
+	}
+	/*if (changedHeight < 600) {
+		mWindowHeight = 600;
+	} else {
+		mWindowHeight = changedHeight + 20;
+	}*/
+	if (changedHeight > 600) {
+		changedWidth = changedWidth * 600 / changedHeight;
+		changedHeight = 600;
+	}
+	mWindowHeight = 600;
+	var mBarHeight = 27;
+	var mFooterWidth = 80;
+	var mFooterHeight = 40;
+	var windowWidth = $(window).width();
+	var windowHeight = $(window).height();
+
+	var fog = $('<div />');
+	fog.attr('id', 'fog');
+	fog.css('position', 'fixed');
+	fog.css('left', 0);
+	fog.css('top', 0);
+	fog.css('background-color', '#FFF');
+	fog.css('filter', 'Alpha(Opacity=30)');
+	fog.css('opacity', '0.5');
+	fog.css('z-index', '10000');
+	fog.css('width', windowWidth);
+	fog.css('height', windowHeight);
+	fog.bind({
+		mouseup: function() {
+			closeWindow();
+		}
+	});
+	$('html').append(fog);
+
+	var mWindow = $('<div id="mWindow" />');
+	mWindow.css('position', 'fixed');
+	var mWindowLeft = (windowWidth - mWindowWidth) / 2, mWindowTop;
+	if (mWindowHeight > windowHeight - 100) {
+		mWindowTop = 20;
+	} else {
+		mWindowTop = (windowHeight - mWindowHeight) / 2;
+	}
+	mWindow.css('background-color', '#FFF');
+	mWindow.css('z-index', '10001');
+	mWindow.css('border-radius', '5px');
+	mWindow.css('box-shadow', '3px 3px 10px #000');
+	mWindow.css('overflow', 's')
+	mWindow.css('width', 0);
+	mWindow.css('height', 0);
+	mWindow.css('left', windowWidth / 2);
+	mWindow.css('top', windowHeight / 2);
+	openWindow();
+	mWindow.css('width', mWindowWidth);
+	mWindow.css('height', mWindowHeight);
+	mWindow.css('left', (windowWidth - mWindowWidth) / 2);
+	mWindow.css('top', mWindowTop);
+	$('html').append(mWindow);
+
+	var mCloseButton = $('<div />');
+	mCloseButton.css('position', 'absolute');
+	mCloseButton.css('left', mWindowWidth - 17.5);
+	mCloseButton.css('top', - 17.5);
+	mCloseButton.css('cursor', 'pointer');
+	mCloseButton.css('background-image', 'url("src/photo_close_normal.png")');
+	mCloseButton.hover(
+		function(e) {
+			$(this).css('background-image', 'url("src/photo_close_hover.png")');
+		},
+		function(e) {
+			$(this).css('background-image', 'url("src/photo_close_normal.png")');
+		});
+	mCloseButton.css('width', 35);
+	mCloseButton.css('height', 35);
+	mCloseButton.bind({
+		mouseup: function(e) {
+			$(this).css('background-image', 'url("src/photo_close_hover.png")');
+			closeWindow();
+		}
+	});
+	mCloseButton.css('display', 'none');
+	mWindow.append(mCloseButton);
+
+	var mPhotoArea = $('<div />');
+	mPhotoArea.css('position', 'absolute');
+	mPhotoArea.css('left', 0);
+	mPhotoArea.css('top', 0);
+	mPhotoArea.css('width', mWindowWidth * 0.75);
+	mPhotoArea.css('height', mWindowHeight);
+	mPhotoArea.css('background-color', '#000');
+	mPhotoArea.css('display', 'none');
+	mWindow.append(mPhotoArea);
+
+	var mPhoto = $('<img src="src/photo/' + arg.srcPath + '"/>');
+	mPhoto.css('position', 'absolute');
+
+	/*if (arg.width <= mPhotoArea.width()) {
+		mPhoto.css('left', (mPhotoArea.width() - arg.width) / 2);
+	} else {
+		mPhoto.css('left', 10);
+		mPhoto.css('width', mPhotoArea.width() - 20);
+		changedHeight = arg.height * (mPhotoArea.width() - 20) / arg.width;
+	}*/
+	mPhoto.css('left', (mPhotoArea.width() - changedWidth) / 2);
+	mPhoto.css('width', changedWidth);
+	mPhoto.css('height', changedHeight);
+	//if (changedHeight <= mPhotoArea.height()) {
+		mPhoto.css('top', (mPhotoArea.height() - changedHeight) / 2);
+	//} else {
+	//	mPhoto.css('top', 20);
+	//}
+	mPhoto.css('display', 'none');
+	mPhotoArea.append(mPhoto);
+
+	var mTextArea = $('<div></div>');
+	mTextArea.css('position', 'absolute');
+	mTextArea.css('width', mWindowWidth * 0.21 - 20);
+	mTextArea.css('height', 450);
+	mTextArea.css('padding', 10);
+	mTextArea.css('font-family', 'Microsoft YaHei');
+	mTextArea.css('font-size', '0.7em');
+	mTextArea.css('')
+	mTextArea.css('left', mWindowWidth * 0.77);
+	mTextArea.css('top', 100);
+	mTextArea.css('background-color', '#f2f2f5');
+	mWindow.append(mTextArea);
+	createLoadingText();
+
+	function createmText(json) {
+		var commentnum = json.comments.length;
+		for (var i = 0; i < commentnum; ++i) {
+			var mText = $('<div class="mText" />');
+			var mTextUser = $('<span style="color: #eb7d5f;">'+ decodeURI(json.comments[i].user, "utf-8") + '网友</span>');
+			var mTextComment = $('<span>：' + decodeURI(json.comments[i].content, "utf-8") + '</span>');
+			var mTextDate = $('<div style="color: #888; margin-top: 10px;">' + json.comments[i].month + '月' + json.comments[i].date + '日</div>');
+			
+			mText.append(mTextUser);
+			mText.append(mTextComment);
+			mText.append(mTextDate);
+			mText.css('margin-top', 20);
+			mText.css('margin-bottom', 20);
+			mTextArea.append(mText);
+		}
+	}
+
+	function createLoadingText() {
+		var mLoadingText = $('<div id="mLoadingText">正在加载中. . .</div>');
+		mTextArea.append(mLoadingText);
+	}
+
+	var mTextPages, nowPage = 0;
+	$.getJSON("https://nero19960329.github.io/json/commentdata.json", function(json) {
+		console.log(json);
+		mTextPages = json.commentpages[arg.id];
+		console.log("textpages: " + mTextPages);
+		if (mTextPages != 0) {;
+			$.getJSON("https://nero19960329.github.io/json/commentdata_" + arg.id + "_0.json", function(json) {
+				$('#mLoadingText').remove();
+				console.log(json);
+				createmText(json);
+				var mPrePageButton = $('<div>上一页</div>');
+				mPrePageButton.css('position', 'absolute');
+				mPrePageButton.css('left', parseInt(mTextArea.css('width')) * 0.5 - 100);
+				mPrePageButton.css('top', 450);
+				mPrePageButton.css('cursor', 'pointer');
+				mPrePageButton.bind({
+					mouseup: function() {
+						//$('#mLoadingText').remove();
+						if (nowPage > 0) {
+							--nowPage;
+						} else if (nowPage == 0) {
+							return;
+						}
+						createLoadingText();
+						console.log("nowPage: " + nowPage + ", mTextPages: " + mTextPages);
+						$('#mNowPage').html('<div id="mNowPage">' + (nowPage + 1) + ' / ' + mTextPages + ' 页</div>');
+						$('.mText').remove();
+						$.getJSON("https://nero19960329.github.io/json/commentdata_" + arg.id + "_" + nowPage + ".json", function(json) {
+							$('#mLoadingText').remove();
+							createmText(json);
+						});
+					}
+				});
+				var mNextPageButton = $('<div>下一页</div>');
+				mNextPageButton.css('position', 'absolute');
+				mNextPageButton.css('left', parseInt(mTextArea.css('width')) * 0.5 + 100);
+				mNextPageButton.css('top', 450);
+				mNextPageButton.css('cursor', 'pointer');
+				mNextPageButton.bind({
+					mouseup: function() {
+						if (nowPage < mTextPages - 1) {
+							++nowPage;
+						} else {
+							return;
+						}
+						createLoadingText();
+						$('#mNowPage').html('<div id="mNowPage">' + (nowPage + 1) + ' / ' + mTextPages + ' 页</div>');
+						console.log("nowPage: " + nowPage + ", mTextPages: " + mTextPages);
+						$('.mText').remove();
+						$.getJSON("https://nero19960329.github.io/json/commentdata_" + arg.id + "_" + nowPage + ".json", function(json) {
+							$('#mLoadingText').remove();
+							createmText(json);
+						});
+					}
+				});
+				var mNowPage = $('<div id="mNowPage">' + (nowPage + 1) + ' / ' + mTextPages + ' 页</div>');
+				mNowPage.css('position', 'absolute');
+				mNowPage.css('left', parseInt(mTextArea.css('width')) * 0.5 - 5);
+				mNowPage.css('top', 450);
+				mTextArea.append(mNowPage);
+				mTextArea.append(mPrePageButton);
+				mTextArea.append(mNextPageButton);
+			});
+		} else {
+			$('#mLoadingText').remove();
+			var mText = $('<div>暂时还没有评论哦~</div>');
+			mText.css('margin-top', 20);
+			mText.css('margin-bottom', 20);
+			mTextArea.append(mText);
+		}
+	});
+
+	function openWindow() {
+		mWindow.css('display', 'block');
+		mWindow.animate({
+			width: mWindowWidth * 1.05,
+			height: mWindowHeight * 1.05,
+			left: (windowWidth - mWindowWidth * 1.05) / 2,
+			top: mWindowTop * 0.95,
+		}, 150, function() {
+			mPhoto.css('display', 'block');
+		});
+
+		mWindow.animate({
+			width: mWindowWidth,
+			height: mWindowHeight,
+			left: mWindowLeft,
+			top: mWindowTop,
+		}, 100, function() {
+			mCloseButton.css('display', 'block');
+			mPhotoArea.css('display', 'block');
+		});
+	}
+
+	function closeWindow() {
+		mPhoto.remove();
+		mTextArea.remove();
+		mWindow.animate({
+			width: 0,
+			height: 0,
+			left: windowWidth / 2,
+			top: windowHeight / 2,
+		}, 150, function() {
+			mWindow.remove();
+		});
+		fog.remove();
+	}
+}
